c8880c79b832b57cfe2087cdf72b2fb5725a1314
AlsoWereMe <alsowereme@outlook.com>
2025-05-23 00:48:13 +0800

    fix: remove duplicate code

--- fuzzer/Fuzzer.py
+++ fuzzer/Fuzzer.py
@@ -23,15 +23,11 @@ class Fuzzer:
     def print_stats(self):
         pass
 
-
-    def run(self, runner: Runner = Runner()) \
-            -> Tuple[subprocess.CompletedProcess, Outcome]:
-
-        def run(
+    def run(
         self, runner: Runner = Runner()
     ) -> Tuple[subprocess.CompletedProcess, Outcome]:
+        """Run `runner` with fuzz input"""
 
-            """Run `runner` with fuzz input"""
         res = runner.run(self.fuzz())
         self.total_execs += 1
         if time.time() - self.last_print_time > 1:
@@ -39,15 +35,11 @@ class Fuzzer:
             self.last_print_time = time.time()
         return res
 
-
-    def runs(self, runner: Runner = Runner(), run_time: int = 60) \
-            -> List[Tuple[subprocess.CompletedProcess, Outcome]]:
-
-        def runs(
+    def runs(
         self, runner: Runner = Runner(), run_time: int = 60
     ) -> List[Tuple[subprocess.CompletedProcess, Outcome]]:
+        """Run `runner` with fuzz input, `trials` times"""
 
-            """Run `runner` with fuzz input, `trials` times"""
         res = list()
         while time.time() - self.start_time < run_time:
             res.append(self.run(runner))
